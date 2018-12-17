63af246ba65b1920c949844fab13450e9a6024f0
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-12-17 15:23:02 +0100

    Adds tile map interaction

--- UI/ViewModel/MainViewModel.cs
+++ UI/ViewModel/MainViewModel.cs
@@ -1,41 +1,68 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.CommandWpf;
+using RelayCommand = GalaSoft.MvvmLight.Command.RelayCommand;
 
 namespace UI.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// See http://www.mvvmlight.net
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        public uint MapWidth => 12 * TileWidth;
-        public uint MapHeight => MapWidth;
-        public uint TileWidth => 40;
-        public uint TileHeight => TileWidth;
+        public static uint NumTilesX => 20;
+        public static uint NumTilesY => NumTilesX;
+        public uint MapWidth => NumTilesX * TileWidth;
+        public uint MapHeight => NumTilesY * TileHeight;
+        public static uint TileWidth => 60;
+        public static uint TileHeight => TileWidth;
 
-        public IEnumerable<Point> Points => Tiles.Keys.Select(p => new Point
-            {X = p.x * TileWidth, Y = p.y * TileHeight, TileWidth = TileWidth, TileHeight = TileHeight});
+        public IEnumerable<Point> Points => Tiles.Values;
 
-        public Dictionary<(uint x, uint y), string> Tiles { get; }
+        private Dictionary<(uint x, uint y), Point> Tiles { get; } = Enumerable.Range(0, (int)NumTilesX).SelectMany(x => Enumerable.Range(0, (int)NumTilesY).Select(y => ((uint)x, (uint)y))).ToDictionary(
+            p => p,
+            p => new Point(p.Item1 * TileWidth, p.Item2 * TileHeight, TileWidth, TileHeight, Brushes.AliceBlue)
+            );
+
 
         public MainViewModel()
         {
-            Tiles = Enumerable.Range(0, 12).SelectMany(x => Enumerable.Range(0, 12).Select(y => ((uint)x, (uint)y))).ToDictionary(x => x, x => x.ToString());
         }
 
-        public struct Point
+        public class Point : ViewModelBase
         {
             public uint X { get; set; }
             public uint Y { get; set; }
             public uint TileWidth { get; set; }
             public uint TileHeight { get; set; }
+            public SolidColorBrush Background { get; set; }
+            public RelayCommand MouseEnterCommand { get; }
+            public RelayCommand MouseDownCommand { get; }
+
+            public Point(uint x, uint y, uint tileWidth, uint tileHeight, SolidColorBrush background)
+            {
+                X = x;
+                Y = y;
+                TileWidth = tileWidth;
+                TileHeight = tileHeight;
+                Background = background;
+                MouseDownCommand = new RelayCommand(ChangeBackground);
+                MouseEnterCommand = new RelayCommand(() =>
+                {
+                    if (Mouse.LeftButton == MouseButtonState.Pressed)
+                    {
+                        ChangeBackground();
+                    }
+                });
+
+                void ChangeBackground()
+                {
+                    Background = Brushes.Black;
+                    RaisePropertyChanged("");
+                }
+            }
+
         }
     }
 }
